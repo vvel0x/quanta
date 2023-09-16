e0f1186b63ec0f053999dce2d86f5c46ec1a9e2f
Issa <vvel0x@users.noreply.github.com>
2023-09-16 05:55:56 +0000

    initial layout

---
+++ src/components/Layout/Shell.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useDisclosure } from "@mantine/hooks";
+import { AppShell, Burger, Group, Skeleton, Text } from "@mantine/core";
+
+export default function Shell({ children }: { children: React.ReactNode }) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Text size="xl" fw={700}>
+            Quanta
+          </Text>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
